822e5577641ba28470fbf68387b6209f51315f1a
Mehmoli <meagulec@gmail.com>
2023-05-30 20:53:35 +0200

    Eerste opzet huiswerk!

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code schrijven voor de hoofd-opdracht
-
+        int [] numeric = {1,2,3,4,5,6,7,8,9,0};
+        String [] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
     }
 }
